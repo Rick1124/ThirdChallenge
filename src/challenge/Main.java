20b13fb8e56670f4e6d3524cc68630d738d53b60
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-22 14:21:18 -0500

    1.1

--- src/challenge/Main.java
+++ src/challenge/Main.java
@@ -1,2 +1,22 @@
-package challenge;public class Main {
+package challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        String sol = MathChallenge(5,5);
+        System.out.println(sol);
+    }
+
+    public static String MathChallenge(int num1, int num2){
+
+        if(num1==num2){
+            return "-1";
+        }
+        else if (num2>num1){
+            return "true";
+        }
+        else {
+            return "false";
+        }
+    }
+
 }
